bfcbf65b02d8f8fa82593d608db01b5959867d2a
gunner <gunnerbrunette1992@gmail.com>
2023-03-17 20:55:59 -0500

    polished it up some more

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://gunnerbrunette.cyou',
+	site: 'https://www.gunnerbrunette.cyou',
 	integrations: [mdx(), sitemap()],
 	output: 'static',
 });
